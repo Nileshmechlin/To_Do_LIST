5b94b4763f3ce961a91c8951f3d5bdc2a757ead1
Nilesh Vijay <135734553+Nileshvijay@users.noreply.github.com>
2024-08-14 20:01:21 +0530

    yes

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
+gj
         axios
             .post('http://localhost:8080/api/user/login', { email, password })
             .then((response) => {
